ea4be7f543b92c19489f5c4623f202af18394b2c
Anton Suprunov <suprunov.anton@gmail.com>
2018-02-11 01:25:33 +0500

    added rest of the filters

--- src/components/Filters/Advanced.js
+++ src/components/Filters/Advanced.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import classnames from 'classnames'
-import { Checkbox } from 'antd'
+import { 
+  Checkbox, 
+  Slider,
+  Input 
+} from 'antd'
 
 import List from './List'
 
@@ -44,6 +48,14 @@ const filtersGroups = [
   }
 ];
 
+const temps = [
+  '2700K',
+  '3500K',
+  '4000K',
+  '5500K',
+  '6000K'
+];
+
 const AdvancedFilters = ({
   onChange
 }) => {
@@ -55,28 +67,51 @@ const AdvancedFilters = ({
         Warmth
         <span className="filters__info"></span>
       </h5>
-
+      <div className="filters__slider">
+        <Slider
+          min={0}
+          max={4}
+          defaultValue={2}
+          tipFormatter={v => temps[v]} 
+        />
+        <span className="filters__note-text">Kelvin</span>
+      </div>
     </div>
 
     <div className="filters__group">
-      <h5 className="filters__group-title">
-        Dim
-        <span className="filters__info"></span>
-      </h5>
-
+      <label for="dim" className="filters__label">
+          <h5 className="filters__group-title">
+            Dim
+            <span className="filters__info"></span>
+        </h5>
+        <div className="filters__filter filters__filter_checkbox">
+          <Checkbox onChange={() => {}} id="dim"></Checkbox>
+        </div>
+      </label>
     </div>
 
     <div className="filters__group">
       <h5 className="filters__group-title">Size (mm)</h5>
-
+      <div className="filters__size">
+        <label for="width" className="filters__input-wrap">
+          <Input id="width" />
+          <span>width</span>
+        </label>
+        <label for="length" className="filters__input-wrap">
+          <Input id="length" />
+          <span>length</span>
+        </label>
+      </div>
     </div>
 
-    {filtersGroups.map((group, index) => (
-      <List 
-        key={`advanced_filter_group_${index}`}
-        {...group}
-      />
-    ))}
+    <div className="filters__advanced-lists">
+      {filtersGroups.map((group, index) => (
+        <List 
+          key={`advanced_filter_group_${index}`}
+          {...group}
+        />
+      ))}
+    </div>
   </div>
 };
 
